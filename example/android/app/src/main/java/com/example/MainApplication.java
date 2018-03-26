7851986d86d14a3d33918dc6fc84a22bbe3b3dff
Maxime Biais <maxime.biais@gmail.com>
2018-03-26 14:34:09 +0200

    Upgrade to RN 0.54

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -27,6 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
 		  new RNRecyclerviewListPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
